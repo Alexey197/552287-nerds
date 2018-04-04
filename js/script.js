748ebbe48f1e3655def42b05113435a8f735f959
Alexey197 <36901478+Alexey197@users.noreply.github.com>
2018-04-04 08:23:38 +0300

    FINAL

--- js/script.js
+++ js/script.js
@@ -12,28 +12,24 @@
       popup.classList.add("modal-show");
       login.focus();
   });
-
-  close.addEventListener("click", function (evt) {
+      close.addEventListener("click", function (evt) {
       evt.preventDefault();
       popup.classList.remove("modal-show");
       popup.classList.remove("modal-error");
-  });
+    });
+      form.addEventListener("submit", function (evt) {
 
-  form.addEventListener("submit", function (evt) {
-
- if (!login.value || !email.value || !message.value) {
-     evt.preventDefault();
-     popup.classList.add("modal-error");
+   if (!login.value || !email.value || !message.value) {
+       evt.preventDefault();
+       popup.classList.add("modal-error");
      }
-  });
-
-  window.addEventListener("keydown", function (evt) {
+    });
+        window.addEventListener("keydown", function (evt) {
       if(evt.keyCode ===27){
         evt.preventDefault() ;
-
-        if (popup.classList.contains("modal-show")) {
+          if (popup.classList.contains("modal-show")) {
             popup.classList.remove("modal-show");
             popup.classList.remove("modal-error");
-        }
-        }
+          }
+          }
   });
